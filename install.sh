446aff2925760d3d986ca3daa33aef5fca8e1a5b
Noam Ross <ross@ecohealthalliance.org>
2019-08-28 14:33:11 -0400

    Add rclone to install script

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 apt-get update -qq && apt-get install -y --no-install-recommends --no-upgrade rsync git ssh wget
 dpkg -l hugo || (wget -q https://github.com/gohugoio/hugo/releases/download/v0.57.2/hugo_0.57.2_Linux-64bit.deb && dpkg -i hugo_0.57.2_Linux-64bit.deb)
+dpkg -l rclone || (wget -q https://github.com/rclone/rclone/releases/download/v1.49.0/rclone-v1.49.0-linux-amd64.deb && dpkg -i rclone-v1.49.0-linux-amd64.deb)
+
 installGithub.r --update FALSE noamross/noamtools
 if [ -f DESCRIPTION ]; then
   Rscript --vanilla -e "devtools::install_dev_deps(dependencies = NA, upgrade = 'never')"
